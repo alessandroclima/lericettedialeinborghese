ee708caa658dbdcdacf65dcc25701dc18a17d5bd
Alessandro Clima <alessandro.clima@philmark.it>
2025-02-08 12:37:24 +0100

    creato progetto in angular con titolo

---
+++ src/app/welcome-page/welcome-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-welcome-page',
+  templateUrl: './welcome-page.component.html',
+  styleUrls: ['./welcome-page.component.css']
+})
+export class WelcomePageComponent {
+  onExploreRecipes() {
+    console.log('L\'utente vuole esplorare le ricette!');
+    // Qui puoi aggiungere la logica per navigare a un'altra pagina o mostrare altre informazioni
+  }
+}
