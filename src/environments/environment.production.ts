68c6c65e41e3bde1454a0ec41aaebfd526f60622
Ale <alessandro.clima@outlook.it>
2025-05-07 15:49:13 +0200

    added environment prod

---
+++ src/environments/environment.production.ts
@@ -0,0 +1,7 @@
+import { Environment } from "./model/environment.model";
+
+
+export const environment: Environment = {
+  production: false,
+  apiBaseUrl: 'https://lericettedialeinborghese-gdfvh7ccgxc9a0e8.italynorth-01.azurewebsites.net'
+};
